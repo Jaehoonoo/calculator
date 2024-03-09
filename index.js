a2c20f9569ec785ee7594675829872e78e282854
Jaehoonoo <justjhoon@gmail.com>
2024-03-09 01:58:04 -0500

    Made a function to handle operator clicks. The first number will get saved, but once an op is clicked, the second num variable gets saved.

--- index.js
+++ index.js
@@ -27,27 +27,50 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     // Manipulates the display of the calculator based on user's click.
-    let wasClicked = false;
+    const display = document.querySelector('.display');
+    let displayEmpty = true;
+    let num1 = '';
+    let operator = '';
+    let num2 = '';
+
     function displayScreen (e) {
         const buttonValue = e.target.textContent;
-
-        const display = document.querySelector('.display');
-        if (!wasClicked) {
-            display.textContent = buttonValue;
-            wasClicked = true;
+        if (operator === '') {
+            if (displayEmpty) {
+                display.textContent = buttonValue;
+                num1 = buttonValue;
+                displayEmpty = false;
+            } else {
+                display.textContent += buttonValue;
+                num1 += buttonValue;
+            }
         } else {
-            display.textContent += buttonValue;
-        }
+            if (display.textContent === '') {
+                display.textContent = buttonValue;
+                num2 = buttonValue;
+            } else {
+                display.textContent += buttonValue;
+                num2 += buttonValue;
+            }
+        };
     };
 
-    let allButtons = document.querySelectorAll('button');
-    allButtons.forEach((button) => {
+    // Updates display.textContent with only numbers by calling displayScreen function.
+    let numBtns = document.querySelectorAll('#num');
+    numBtns.forEach((button) => {
         button.addEventListener('click', displayScreen);
     });
 
-
-    const num1 = 3;
-    const num2 = 5;
-    const operator = '+';
-
+    // Function to handle operator button click
+    function operatorClicked(op) {
+        operator = op;
+        display.textContent = '';
+    }
+    const operatorButtons = document.querySelectorAll('#operator');
+    operatorButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            operatorClicked(button.dataset.operator);
+        });
+    });
+        
 });
